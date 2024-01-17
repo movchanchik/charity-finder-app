bf7757b6bd453cb284ac66c29ffb03460a431676
movchanchik <inna0992@gmail.com>
2024-01-16 22:11:47 -0500

    try to fix render bug

---
+++ app/components/FavoritesList.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import CharityItem from "../components/CharityItem";
+import { LocalStorageItem } from "../types/inrefaces";
+
+const FavoritesList = () => {
+  const localStorageItems = JSON.parse(
+    localStorage.getItem("favoriteList") || "[]"
+  );
+  return (
+    <>
+      {!localStorageItems.length ? (
+        <div className="text-xl font-semibold text-gray-400 flex flex-row justify-center p-4 my-4">
+          No Favorites Yet
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
+          {localStorageItems?.map((localStorageItem: LocalStorageItem) => (
+            <CharityItem key={localStorageItem.ein} item={localStorageItem} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FavoritesList;
